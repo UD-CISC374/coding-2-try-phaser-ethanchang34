2f54775a890cbc05c77b0479532eb1ce16791618
ethanchang34 <ethanchang34@yahoo.com>
2020-02-25 20:41:23 -0500

    Added moving background

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,45 +2,55 @@ import ExampleObject from '../objects/exampleObject';
 import { GameObjects } from 'phaser';
 
 export default class MainScene extends Phaser.Scene {
-  background: Phaser.GameObjects.TileSprite;
+  //background: Phaser.GameObjects.TileSprite;
+  mt_back: Phaser.GameObjects.TileSprite;
+  mt_mid: Phaser.GameObjects.TileSprite;
+  mt_front: Phaser.GameObjects.TileSprite;
 /*   ship1: Phaser.GameObjects.Image;
   ship2: Phaser.GameObjects.Image;
   ship3: Phaser.GameObjects.Image; */
-  golem: Phaser.GameObjects.Sprite;
-  golemDie: Phaser.GameObjects.Sprite;
+  /* golem: Phaser.GameObjects.Sprite;
+  golemDie: Phaser.GameObjects.Sprite; */
+  /* apple: Phaser.GameObjects.Image;
+  pear: Phaser.GameObjects.Image; */
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
-    this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, "background");
-    this.background.setOrigin(0, 0);
+    this.mt_back = this.add.tileSprite(0, 0, 0, 0, "mt_back");
+    this.mt_back.setOrigin(0, 0);
+    this.mt_mid = this.add.tileSprite(0, 100, 0, 0, "mt_mid");
+    this.mt_mid.setOrigin(0, 0);
+    this.mt_front = this.add.tileSprite(0, 500, 0, 0, "mt_front");
+    this.mt_front.setOrigin(0, 0);
 
-    /* this.ship1 = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2, "ship");
-    this.ship2 = this.add.image(this.scale.width / 2, this.scale.height / 2, "ship2");
-    this.ship3 = this.add.image(this.scale.width / 2 + 50, this.scale.height / 2, "ship3"); */
-    this.golem = this.add.sprite(this.scale.width / 2 - 100, this.scale.height / 2, "golem");
+    /*this.apple = this.add.image(this.scale.width / 2 - 100, this.scale.height / 2, "apple");
+    this.pear = this.add.image(this.scale.width / 2, this.scale.height / 2, "pear");
+    this.apple.setScale(0.05);
+    this.pear.setScale(0.05); */
 
-    this.anims.create({
+    //this.golem = this.add.sprite(this.scale.width / 2 - 100, this.scale.height / 2, "golem");
+    /* this.anims.create({
       key: "golem_anim",
-      frames: this.anims.generateFrameNumbers("golem"),
-      frameRate: 20,
+      frames: this.anims.generateFrameNumbers("golem", {}),
+      frameRate: 50,
       repeat: -1
     });
     this.anims.create({
       key: "golem_die_anim", 
-      frames: this.anims.generateFrameNumbers("golemDie"),
+      frames: this.anims.generateFrameNumbers("golemDie", {}),
       frameRate: 20,
       repeat: 0,
       hideOnComplete: true
-    });
+    }); */
 
-    this.golem.play("golem_anim");
+    //this.golem.play("golem_anim");
 
-    this.golem.setInteractive();
+    //this.golem.setInteractive();
 
-    this.input.on('gameobjectdown', this.destroyGolem, this);
+    //this.input.on('gameobjectdown', this.destroyGolem, this);
 
     /* this.ship1.setScale(2);
     this.ship1.flipY = true; */
@@ -49,29 +59,31 @@ export default class MainScene extends Phaser.Scene {
     this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
   }
 
-  moveShip(ship, speed) {
+  /* moveShip(ship, speed) {
     ship.y += speed;
     if (ship.y > this.scale.height) {
       this.resetShipPos(ship);
     }
-  }
+  } */
 
-  resetShipPos(ship) {
+  /* resetShipPos(ship) {
     ship.y = 0;
     var randomX = Phaser.Math.Between(0, this.scale.width);
     ship.x = randomX;
-  }
+  } */
 
-  destroyGolem(pointer, gameObject) {
+  /* destroyGolem(pointer, gameObject) {
     gameObject.setTexture("golemDie");
     gameObject.play("golem_die_anim");
-  }
+  } */
 
   update() {
     /* this.moveShip(this.ship1, 1);
     this.moveShip(this.ship2, 2);
     this.moveShip(this.ship3, 3); */
-    this.moveShip(this.golem, 3); 
-    this.background.tilePositionY -= 0.5;
+    //this.moveShip(this.golem, 3); 
+    this.mt_back.tilePositionX += 2;
+    this.mt_mid.tilePositionX += 3;
+    this.mt_front.tilePositionX += 4;
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,18 +4,17 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("background", "assets/images/background.jpg");
-    this.load.image("ship", "assets/images/ship.jpg");
-    this.load.image("ship2", "assets/images/ship2.jpg");
-    this.load.image("ship3", "assets/images/ship3.jpg");
-    this.load.spritesheet("golem", "assets/spriteSheets/golem-walk.png", {
-      frameWidth: 16, 
-      frameHeight: 16
-    });
-    this.load.spritesheet("golemDie", "assets/spriteSheets/golem-die.png", {
+    //this.load.image("background", "assets/images/background.png");
+    this.load.image("mt_back", "assets/images/mt_back.png");
+    this.load.image("mt_mid", "assets/images/mt_mid.png");
+    this.load.image("mt_front", "assets/images/mt_front.png");
+    this.load.image("apple", "assets/images/apple.png");
+    this.load.image("pear", "assets/images/pear.png");
+
+    /* this.load.spritesheet("golemDie", "assets/spriteSheets/golem-die.png", {
       frameWidth: 16,
       frameHeight: 16
-    });
+    }); */
   }
 
   create() {
