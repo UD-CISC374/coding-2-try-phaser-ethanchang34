31497d9f84744a68ad9e7b2648ac5caa335f2148
ethanchang34 <ethanchang34@yahoo.com>
2020-03-04 00:37:22 -0500

    Updates

---
+++ beta/precache-manifest.b2deeda8c90b56b2978fc87c1f00340c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0908185da9a9812ae7b2feb7283e599",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/font/font.xml"
+  },
+  {
+    "revision": "82ad670d8508ff860b5be29b277c887a",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/Aincrad.jpg"
+  },
+  {
+    "revision": "ff4c70f32fc186785da545a918d29183",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/apple.png"
+  },
+  {
+    "revision": "1df6d954c606d53a9cae801ac07e5b8b",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/boss.png"
+  },
+  {
+    "revision": "c22334b2d40ac1aa3dfc9b0ca18476ab",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/mt_back.png"
+  },
+  {
+    "revision": "549c8e9eafac660cd2ba485145d5e1d3",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/mt_front.png"
+  },
+  {
+    "revision": "7c52fc858ffdf32e2a5a0b88d29bf728",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/mt_mid.png"
+  },
+  {
+    "revision": "a595ee9f65d2845d9261cbc60c45fb08",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/pear.png"
+  },
+  {
+    "revision": "ab7f8f10079bb729fb432eba59e50c85",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/ruby.png"
+  },
+  {
+    "revision": "16c2cf267e9ee7d697e2bc9d183ab230",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/images/sky.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-ethanchang34/betaassets/spriteSheets/ship3.png"
+  },
+  {
+    "revision": "80c84cc3f12c01ad984b",
+    "url": "/coding-2-try-phaser-ethanchang34/betagen.main.7c274923e32c545ecc4c.bundle.js"
+  },
+  {
+    "revision": "91fa08b4145d5d708c39",
+    "url": "/coding-2-try-phaser-ethanchang34/betagen.vendors.ff5cbb39cff73568fd06.bundle.js"
+  },
+  {
+    "revision": "2f7557587d386342889f976f290d4ebd",
+    "url": "/coding-2-try-phaser-ethanchang34/betaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-ethanchang34/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-ethanchang34/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-ethanchang34/betaprecache-manifest.b2deeda8c90b56b2978fc87c1f00340c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
